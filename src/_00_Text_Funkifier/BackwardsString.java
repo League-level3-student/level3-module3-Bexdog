de14850145d98803ceda2ba9fdbf2db45fdb32ab
beckh <beckh@McKid>
2020-03-29 11:28:12 -0700

    test special string(IP)

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,11 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		
-		return "";
+		String done = "";
+		for(int i = s.length()-1; i>=0;i--) {
+			done+=s.charAt(i);
+		}
+		return done;
 	}
 
 }
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,8 +8,9 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
-		
-		return "";
+		s=s.toLowerCase();
+		s.substring(0, 0).toUpperCase();
+		return s;
 	}
 
 }
